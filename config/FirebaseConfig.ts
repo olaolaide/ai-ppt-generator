71f298170d1638ea30975a32c4e3dd8be5c43cd7
ola <olaolaide@outlook.com>
2025-10-22 08:53:55 +0100

    Added firebase to workspace to save user data

--- config/FirebaseConfig.ts
+++ config/FirebaseConfig.ts
@@ -1,18 +1,19 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
-import {getAnalytics} from "firebase/analytics";
+import {getFirestore} from "firebase/firestore";
+
 
 const firebaseConfig = {
     apiKey: import.meta.env.FIREBASE_API_KEY,
-    authDomain:  import.meta.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL:  import.meta.env.FIREBASE_DATABASE_URL,
-    projectId:  import.meta.env.FIREBASE_PROJECT_ID,
-    storageBucket:  import.meta.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId:  import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId:  import.meta.env.FIREBASE_APP_ID,
-    measurementId:  import.meta.env.FIREBASE_MEASUREMENT_ID
+    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.FIREBASE_APP_ID,
+    measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const firebaseDb = getFirestore(app)
--- src/workspace/index.tsx
+++ src/workspace/index.tsx
@@ -1,10 +1,34 @@
 import {Link, Outlet} from "react-router";
 import {useUser} from "@clerk/clerk-react";
 import {Button} from "@/components/ui/button.tsx";
+import {firebaseDb} from "../../config/FirebaseConfig.ts";
+import {doc, getDoc, serverTimestamp, setDoc} from "firebase/firestore";
+import {useEffect} from "react";
 
 function Workspace() {
-    const {user} = useUser()
-    if (!user) {
+    const {user, isLoaded} = useUser()
+
+    useEffect(() => {
+        user && createUser()
+    }, [user]);
+
+    const createUser = async () => {
+        // if user exist in the database
+        const docRef = doc(firebaseDb, 'users', user?.primaryEmailAddress?.emailAddress ?? '')
+        const docSnap = await getDoc(docRef)
+        if (docSnap.exists()) {
+            console.log("Document data: ", docSnap.data())
+        } else {
+            // Insert new user
+            await setDoc(doc(firebaseDb, 'users', user?.primaryEmailAddress?.emailAddress ?? ''), {
+                fullName: user?.fullName,
+                emailAddress: user?.primaryEmailAddress?.emailAddress,
+                createdAt: serverTimestamp(),
+                credits: 2
+            })
+        }
+    }
+    if (!user && isLoaded) {
         return (
             <div>
                 Please sign in to access workspace
